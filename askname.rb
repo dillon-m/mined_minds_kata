f9b2e126a0ca8cefd33956ea96103e17a990f0e5
dillon-m <dillon5079@yahoo.com>
2017-06-26 13:41:29 -0400

    askname

---
+++ askname.rb
@@ -0,0 +1,13 @@
+print "What's your first name?"
+first_name = gets.chomp
+
+print "What's your last name?"
+last_name = gets.chomp
+
+print "What city do you live in?" 
+city = gets.chomp
+
+print "What state/province do you live in?"
+state = gets.chomp
+
+puts "Hello, #{first_name} #{last_name}. I see you live in #{city}, #{state}."
